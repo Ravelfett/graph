c9f8fa6228feae197fc26a1140fa4c8086f88675
Ravelfett <ravelfett@gmail.com>
2022-01-19 19:07:39 +0100

    Revert "Add files via upload"
    This reverts commit db80a464069101bcddb97f443ce010b2ab02f1df.
    
    	modified:   graf.js
    	modified:   main.js

--- graf.js
+++ graf.js
@@ -1,30 +1,48 @@
 class Graph {
   constructor() {
-    this.nodes = [];
+    this.nodes = {};
+    this.colors = {};
   }
   addNode(n) {
-    let id = Math.random();
-    n.id = id;
-    n.graph = this;
-    this.nodes[id] = n
-    return id;
+    this.nodes[n.id] = n;
+    return n;
+  }
+  addConnection(n1, n2) {
+    this.nodes[n1].neighbors.push(n2);
+    this.nodes[n2].neighbors.push(n1);
+  }
+  color() {
+    this.colors = {};
+    let c = 0;
+    for(const i in this.nodes) {
+      if (!(i in this.colors)){
+        this.colors[i] = c;
+      }
+      for(const j in this.nodes) {
+        if (i!=j) {
+          if (!(j in this.colors)){
+            let yes = true;
+            for (const k of this.nodes[j].neighbors) {
+              if(k in this.colors){
+                if (this.colors[k] == c) {
+                  yes = false;
+                }
+              }
+            }
+            if(yes) {
+              this.colors[j] = c;
+            }
+          }
+        }
+      }
+      c++;
+    }
   }
 }
 
 class Node {
   constructor() {
-    this.id = null;
-    this.graph = null;
-    this.connections = [];
-  }
-  connect(connection, other){
-    connection.target = other;
-    this.connections.push(connection);
-  }
-}
-
-class Connection {
-  constructor() {
-    this.target = null;
+    this.id = Math.random();
+    this.neighbors = [];
   }
 }
--- main.js
+++ main.js
@@ -41,9 +41,11 @@ document.onmousedown = function (e) {
     if (!clicks[0]) {
       clicks[0] = true;
       const n = new VNode();
+      clicking = n.id;
       n.x = mouse[0];
       n.y = mouse[1];
-      clicking = graf.addNode(n);
+      graf.addNode(n);
+      graf.color();
     }
   }
   if (e.button == 2) {
@@ -64,11 +66,8 @@ document.onmouseup = function (e) {
     if (clicks[1]) {
       for(const i in graf.nodes){
         if((graf.nodes[i].x-mouse[0])**2+(graf.nodes[i].y-mouse[1])**2 < 60**2) {
-          c1 = new VConnection(1);
-          c2 = new VConnection(1);
-          graf.nodes[clicking].connect(c1, i);
-          graf.nodes[i].connect(c2, clicking);
-		  calc();
+          graf.addConnection(clicking, i);
+          graf.color();
         }
       }
     }
@@ -79,7 +78,6 @@ document.onmouseup = function (e) {
 class VNode extends Node {
   constructor() {
     super();
-    this.color = 'hsl(199, 100%, 50%)';
     this.x = Math.random()*width;
     this.y = Math.random()*height;
     this.vx = 0;
@@ -97,136 +95,22 @@ class VNode extends Node {
     this.ax = 0;
     this.ay = 0;
   }
-  render(ctx){
-
-  }
-}
-
-class VConnection extends Connection {
-  constructor(weight) {
-    super();
-    this.color = "rgb(189, 152, 109)";
-    this.weight = weight;
-  }
 }
 
 const mouse = [0, 0];
 const clicks = [false, false];
 let clicking = 0;
 
-function pointsToEquation(p1x, p1y, p2x, p2y){
-  let dif = p2y - p1y;
-  let a = (p2x-p1x)/dif;
-  let b = p1y-(a*p1x);
-  return [a, b];
-}
-
-function intersection(l1, l2){
-  let x = (l2[1]-l1[1])/(l1[0]-l2[0]);
-  return [x, l1[0]*x+l1[1]];
-}
-
-function intersects(p1x, p1y, p2x, p2y, p3x, p3y, p4x, p4y){
-  let lines = [pointsToEquation(p1x, p1y, p2x, p2y), pointsToEquation(p3x, p3y, p4x, p4y)];
-  let inter = intersection(lines[0], lines[1]);
-  let x = inter[0]
-  return ((x<p1x)^(x<p2x)&&(x<p3x)^(x<p4x));
-}
+const colors = [263];
 
 const graf = new Graph();
-ns = []
-for(let i = 0; i < 7; i++){
-  let n = new VNode();
-  /*n.x = Math.cos(i/20*Math.PI*2)*300+width/2;
-  n.y = Math.sin(i/20*Math.PI*2)*300+height/2;*/
-  ns.push(graf.addNode(n))
-}
-for(let i = 0; i < 14; i++){
-  let n1 = ns[Math.floor(Math.random()*ns.length)]
-  let n2 = ns[Math.floor(Math.random()*ns.length)]
-  let node1 = graf.nodes[n1];
-  let node2 = graf.nodes[n2];
-  let inttt = false;
-  for(let j in graf.nodes){
-	let n3 = graf.nodes[j];
-	if(!(n3 == n1 || n3 == n2)){
-	  for(let k of n3.connections){
-		let n4 = graf.nodes[k.target];
-		if(!(n4 == n1 || n4 == n2)){
-		 let inttttt = intersects(node1.x, node1.y, node2.x, node2.y, n3.x, n3.y, n4.x, n4.y);
-		  //console.log(Math.floor(node1.x), Math.floor(node1.y), Math.floor(node2.x), Math.floor(node2.y), Math.floor(n3.x), Math.floor(n3.y), Math.floor(n4.x), Math.floor(n4.y), inttttt)
-		  if(inttttt){
-		    inttt = true;
-		  }
-		}
-	  }
-	}
-  }
-  console.log(node1, node2)
-  if(!inttt){
-    let w = Math.floor(Math.random()*5)
-    c1 = new VConnection(w);
-    c2 = new VConnection(w);
-    node1.connect(c1, n2);
-    node2.connect(c2, n1);
-  }
-  //debugger
-}
-
-
-function path(graf, n1, n2){
-  let l = {};
-  for(let i in graf.nodes){
-    l[i] = {distance: null, visited: false, path: []};
-  }
-  l[n1].distance = 0;
-  while(true){
-    let min = null;
-    for(let i in l){
-      if(l[i].distance != null && !l[i].visited){
-        if(min == null){
-          min = i;
-        }
-        if(l[min]>l[i]){
-          min = i;
-        }
-      }
-    }
-    if (min == null){
-      return {distance: -1, path: []};
-    }
-    if (min == n2) {
-      return l[min];
-    }
-    for(let i of graf.nodes[min].connections){
-      let ndist = l[min].distance+i.weight;
-      if(l[i.target].distance == null){
-        l[i.target].distance = ndist;
-        l[i.target].path = [...l[min].path, min];
-      }
-      if (l[i.target].distance>ndist) {
-        l[i.target].distance = ndist;
-        l[i.target].path = [...l[min].path, min];
-      }
-    }
-    l[min].visited = true;
-  }
-}
-
-function calc(){
-  for(let i in graf.nodes){
-    graf.nodes[i].color = "hsl(199, 100%, 50%)";
-  }
-  graf.nodes[ns[0]].color = "rgb(184, 222, 78)";
-  graf.nodes[ns[3]].color = "rgb(14, 67, 138)";
-  let paf = path(graf, ns[0], ns[3]);
-  for(let i of paf.path.slice(1)){
-    graf.nodes[i].color = "red"
-  }
-}
-
-calc();
-
+const n1 = graf.addNode(new VNode());
+const n2 = graf.addNode(new VNode());
+const n3 = graf.addNode(new VNode());
+graf.addConnection(n1.id, n2.id);
+graf.addConnection(n2.id, n3.id);
+graf.addConnection(n3.id, n1.id);
+graf.color();
 
 function animate() {
   context.clearRect(0, 0, width, height);
@@ -238,17 +122,14 @@ function animate() {
 
 
   for(const i in graf.nodes){
-    let node1 = graf.nodes[i]
-    for(const j of node1.connections){
-      let node2 = graf.nodes[j.target];
-	  continue
-      if ((node1.x-node2.x)**2+(node1.y-node2.y)**2 > 800**2) {
-        node1.ax += (node2.x-node1.x)/4000;
-        node1.ay += (node2.y-node1.y)/4000;
+    for(const j of graf.nodes[i].neighbors){
+      if ((graf.nodes[i].x-graf.nodes[j].x)**2+(graf.nodes[i].y-graf.nodes[j].y)**2 > 800**2) {
+        graf.nodes[i].ax += (graf.nodes[j].x-graf.nodes[i].x)/4000;
+        graf.nodes[i].ay += (graf.nodes[j].y-graf.nodes[i].y)/4000;
       }
-      if ((node1.x-node2.x)**2+(node1.y-node2.y)**2 < 300**2) {
-        node1.ax += (node1.x-node2.x)/4000;
-        node1.ay += (node1.y-node2.y)/4000;
+      if ((graf.nodes[i].x-graf.nodes[j].x)**2+(graf.nodes[i].y-graf.nodes[j].y)**2 < 300**2) {
+        graf.nodes[i].ax += (graf.nodes[i].x-graf.nodes[j].x)/4000;
+        graf.nodes[i].ay += (graf.nodes[i].y-graf.nodes[j].y)/4000;
       }
     }
   }
@@ -261,41 +142,63 @@ function animate() {
     graf.nodes[i].update();
   }
 
+  context.strokeStyle = "rgb(189, 152, 109)";
   for(const i in graf.nodes){
-    for(const j of graf.nodes[i].connections){
-      context.strokeStyle = j.color;
+    for(const j of graf.nodes[i].neighbors){
       context.beginPath();
       context.lineWidth = 6;
       context.moveTo(graf.nodes[i].x, graf.nodes[i].y);
-      context.lineTo(graf.nodes[j.target].x, graf.nodes[j.target].y);
+      context.lineTo(graf.nodes[j].x, graf.nodes[j].y);
+      context.stroke();
+      context.closePath();
+    }
+    if(clicks[1]){
+      context.beginPath();
+      context.lineWidth = 2;
+      context.moveTo(graf.nodes[clicking].x, graf.nodes[clicking].y);
+      context.lineTo(mouse[0], mouse[1]);
       context.stroke();
       context.closePath();
     }
   }
-  if(clicks[1]){
-    context.beginPath();
-    context.lineWidth = 2;
-    context.moveTo(graf.nodes[clicking].x, graf.nodes[clicking].y);
-    context.lineTo(mouse[0], mouse[1]);
-    context.stroke();
-    context.closePath();
-  }
 
   for(const i in graf.nodes){
     context.beginPath();
-    context.fillStyle = graf.nodes[i].color;
+    if (i in graf.colors) {
+      if (colors.length<=graf.colors[i]) {
+        interval = 130;
+        let col = 0;
+        let closest = 0;
+        do {
+          col = Math.floor(Math.random()*255);
+          closest = colors.reduce(function(prev, curr) {
+            return (Math.abs(curr - col) < Math.abs(prev - col) ? curr : prev);
+          });
+          interval /=1.01
+          console.log(closest, col);
+        } while (Math.abs(closest-col)<interval)
+        colors.push(col);
+      }
+      context.fillStyle = 'hsl('+ colors[graf.colors[i]] +',100%,50%)';
+    }else{
+      context.fillStyle = "white";
+    }
     context.arc(graf.nodes[i].x, graf.nodes[i].y, 20, 0, 2 * Math.PI);
     context.fill();
     context.closePath();
   }
-  context.fillStyle = "rgb(163, 180, 231)";
-  context.font = '48px consolas';
-  context.textAlign = "center";
-  context.textBaseline = "middle";
-  for(const i in graf.nodes){
-    for(const j of graf.nodes[i].connections){
-      context.fillText(j.weight, (graf.nodes[i].x+graf.nodes[j.target].x)/2, (graf.nodes[i].y+graf.nodes[j.target].y)/2);
-    }
+
+  context.font = "50px Arial";
+  context.fillText(colors.length, 30, 60)
+  for (let c in colors){
+    context.beginPath();
+    context.fillStyle = 'hsl('+ colors[c] +',100%,50%)';
+    context.strokeStyle = "rgb(99, 99, 99)";
+    context.lineWidth = 2;
+    context.rect(0, 100+30*c, 60, 30);
+    context.fill();
+    context.stroke();
+    context.closePath();
   }
   window.requestAnimationFrame(animate);
 }
