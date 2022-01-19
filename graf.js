db80a464069101bcddb97f443ce010b2ab02f1df
Ravelfett <41432535+Ravelfett@users.noreply.github.com>
2022-01-19 09:53:36 +0100

    Add files via upload

--- graf.js
+++ graf.js
@@ -1,48 +1,30 @@
 class Graph {
   constructor() {
-    this.nodes = {};
-    this.colors = {};
+    this.nodes = [];
   }
   addNode(n) {
-    this.nodes[n.id] = n;
-    return n;
-  }
-  addConnection(n1, n2) {
-    this.nodes[n1].neighbors.push(n2);
-    this.nodes[n2].neighbors.push(n1);
-  }
-  color() {
-    this.colors = {};
-    let c = 0;
-    for(const i in this.nodes) {
-      if (!(i in this.colors)){
-        this.colors[i] = c;
-      }
-      for(const j in this.nodes) {
-        if (i!=j) {
-          if (!(j in this.colors)){
-            let yes = true;
-            for (const k of this.nodes[j].neighbors) {
-              if(k in this.colors){
-                if (this.colors[k] == c) {
-                  yes = false;
-                }
-              }
-            }
-            if(yes) {
-              this.colors[j] = c;
-            }
-          }
-        }
-      }
-      c++;
-    }
+    let id = Math.random();
+    n.id = id;
+    n.graph = this;
+    this.nodes[id] = n
+    return id;
   }
 }
 
 class Node {
   constructor() {
-    this.id = Math.random();
-    this.neighbors = [];
+    this.id = null;
+    this.graph = null;
+    this.connections = [];
+  }
+  connect(connection, other){
+    connection.target = other;
+    this.connections.push(connection);
+  }
+}
+
+class Connection {
+  constructor() {
+    this.target = null;
   }
 }
